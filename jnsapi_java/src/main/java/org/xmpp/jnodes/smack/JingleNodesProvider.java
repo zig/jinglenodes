a58c0ff9e6d758f7e9a4af5f195d204de289e290
barata7 <barata7@gmail.com>
2009-10-27 21:39:05 +0000

    Relay Channel UnitTest and concurrency fixes

--- jnsapi_java/src/main/java/org/xmpp/jnodes/smack/JingleNodesProvider.java
+++ jnsapi_java/src/main/java/org/xmpp/jnodes/smack/JingleNodesProvider.java
@@ -3,12 +3,20 @@ package org.xmpp.jnodes.smack;
 import org.jivesoftware.smack.provider.IQProvider;
 import org.jivesoftware.smack.packet.IQ;
 import org.xmlpull.v1.XmlPullParser;
+import org.xmpp.jnodes.RelayChannel;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.io.IOException;
 
 public class JingleNodesProvider implements IQProvider {
 
     public static final String NAME = "candidate";
     public static final String NAMESPACE = "http://jabber.org/protocol/jinglenodes#channel";
     public static final String UDP = "udp";
+    private final ConcurrentHashMap<String, RelayChannel> channels = new ConcurrentHashMap<String, RelayChannel>();
+
+    private final AtomicInteger ids = new AtomicInteger(0);
 
     public IQ parseIQ(final XmlPullParser parser) throws Exception {
 
@@ -44,7 +52,32 @@ public class JingleNodesProvider implements IQProvider {
         return iq;
     }
 
-    private void createUdpChannel(){
+    private IQ createUdpChannel(final IQ iq) {
+
+        try {
+            final RelayChannel rc = RelayChannel.createLocalRelayChannel();
+
+            final IQ result = new IQ() {
+                public String getChildElementXML() {
+                    final StringBuilder str = new StringBuilder();
+                    str.append("<").append(NAME).append(" xmlns='").append(NAMESPACE).append("' protocol='").append(UDP).append("' id='").append(ids.incrementAndGet()).append("' ");
+                    str.append("ip='").append(rc.getIp()).append("' ");
+                    str.append("porta='").append(rc.getPortA()).append("' portb='").append(rc.getPortB()).append("'/>");
+                    return str.toString();
+                }
+            };
+
+            result.setPacketID(iq.getPacketID());
+            result.setFrom(iq.getTo());
+            result.setTo(iq.getFrom());
+            result.setType(IQ.Type.RESULT);
+
+            return result;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return JingleChannelRequest.createEmptyError(iq);
+        }
 
     }
 
---
+++ jnsapi_java/src/test/java/org/xmpp/jnodes/smack/JingleNodesProviderTest.java
@@ -0,0 +1,17 @@
+package org.xmpp.jnodes.smack;
+
+import junit.framework.TestCase;
+
+public class JingleNodesProviderTest extends TestCase {
+
+
+
+    public void testCreationResult(){
+
+        final JingleNodesProvider provider = new JingleNodesProvider();
+
+        
+
+
+    }
+}
