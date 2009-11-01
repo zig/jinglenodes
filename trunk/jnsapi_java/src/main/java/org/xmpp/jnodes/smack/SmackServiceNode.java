bdbd70eefc112c48a8ebbf3914e8e9fe9dd511c9
barata7@gmail.com <barata7@gmail.com@1b8834d4-a9fb-11de-8c46-7bd654735883>
2009-11-01 13:31:02 +0000

    [9] - Pure Java Services API

--- trunk/jnsapi_java/src/main/java/org/xmpp/jnodes/smack/SmackServiceNode.java
+++ trunk/jnsapi_java/src/main/java/org/xmpp/jnodes/smack/SmackServiceNode.java
@@ -2,6 +2,7 @@ package org.xmpp.jnodes.smack;
 
 import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.filter.PacketIDFilter;
 import org.jivesoftware.smack.packet.IQ;
 import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smack.provider.ProviderManager;
@@ -43,12 +44,25 @@ public class SmackServiceNode implements ConnectionListener, PacketListener {
         }, timeout, timeout, TimeUnit.MILLISECONDS);
 
         final ConnectionConfiguration conf = new ConnectionConfiguration(server, port);
+        conf.setSASLAuthenticationEnabled(false);
+        conf.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
         connection = new XMPPConnection(conf);
     }
 
     public void connect(final String user, final String password) throws XMPPException {
+        connect(user, password, false);
+    }
+
+    public void connect(final String user, final String password, final boolean tryCreateAccount) throws XMPPException {
         connection.connect();
         connection.addConnectionListener(this);
+        if (tryCreateAccount) {
+            try {
+                connection.getAccountManager().createAccount(user, password);
+            } catch (final XMPPException e) {
+                // Do Nothing as account may exists
+            }
+        }
         connection.login(user, password);
         ServiceDiscoveryManager.getInstanceFor(connection).addFeature(JingleChannelIQ.NAMESPACE);
         connection.addPacketListener(this, new PacketFilter() {
@@ -90,25 +104,25 @@ public class SmackServiceNode implements ConnectionListener, PacketListener {
 
     }
 
-    protected IQ createUdpChannel() {
+    protected IQ createUdpChannel(final JingleChannelIQ iq) {
 
         try {
             final RelayChannel rc = RelayChannel.createLocalRelayChannel();
             final int id = ids.incrementAndGet();
             final String sId = String.valueOf(id);
-
             rc.setAttachment(sId);
-            final IQ result = new IQ() {
-                public String getChildElementXML() {
-                    final StringBuilder str = new StringBuilder();
-                    str.append("<").append(JingleChannelIQ.NAME).append(" xmlns='").append(JingleChannelIQ.NAMESPACE).append("' protocol='").append(JingleChannelIQ.Protocol.udp).append("' id='").append(sId).append("' ");
-                    str.append("ip='").append(rc.getIp()).append("' ");
-                    str.append("porta='").append(rc.getPortA()).append("' portb='").append(rc.getPortB()).append("'/>");
-                    return str.toString();
-                }
-            };
 
+            channels.put(sId, rc);
+
+            final JingleChannelIQ result = new JingleChannelIQ();
             result.setType(IQ.Type.RESULT);
+            result.setTo(iq.getFrom());
+            result.setFrom(iq.getTo());
+            result.setPacketID(iq.getPacketID());
+            result.setHost(rc.getIp());
+            result.setPorta(rc.getPortA());
+            result.setPortb(rc.getPortB());
+            result.setId(sId);
 
             return result;
 
@@ -122,10 +136,35 @@ public class SmackServiceNode implements ConnectionListener, PacketListener {
     public void processPacket(final Packet packet) {
 
         System.out.println("Received: " + packet.toXML());
+        if (packet instanceof JingleChannelIQ) {
+            final JingleChannelIQ request = (JingleChannelIQ) packet;
+            if (request.isRequest()) {
+                connection.sendPacket(createUdpChannel(request));
+            }
+        }
 
     }
 
     public XMPPConnection getConnection() {
         return connection;
+    }
+
+    public static JingleChannelIQ getChannel(final XMPPConnection xmppConnection, final String serviceNode) {
+        if (xmppConnection == null || !xmppConnection.isConnected()) return null;
+
+        final JingleChannelIQ iq = new JingleChannelIQ();
+        iq.setFrom(xmppConnection.getUser());
+        iq.setTo(serviceNode);
+
+        PacketCollector collector = xmppConnection.createPacketCollector(new PacketIDFilter(iq.getPacketID()));
+        xmppConnection.sendPacket(iq);
+        JingleChannelIQ result = (JingleChannelIQ) collector.nextResult(Math.round(SmackConfiguration.getPacketReplyTimeout() * 1.5));
+        collector.cancel();
+
+        return result;
+    }
+
+    ConcurrentHashMap<String, RelayChannel> getChannels() {
+        return channels;
     }
 }
