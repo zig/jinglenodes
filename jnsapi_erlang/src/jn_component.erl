6f4796271100e37c53fe07e7bcc6a26940b96aa9
barata7 <barata7@gmail.com>
2009-11-21 01:27:50 +0000

    Added Services Support

--- jnsapi_erlang/src/jn_component.erl
+++ jnsapi_erlang/src/jn_component.erl
@@ -3,6 +3,8 @@
 -define(NS_CHANNEL,'http://jabber.org/protocol/jinglenodes#channel').
 -define(NAME_CHANNEL,'candidate').
 -define(NS_JINGLE_NODES_s,"http://jabber.org/protocol/jinglenodes").
+-define(NS_JINGLE_NODES,'http://jabber.org/protocol/jinglenodes').
+-define(NAME_SERVICES,'services').
 -define(NS_CHANNEL_s,"http://jabber.org/protocol/jinglenodes#channel").
 
 -include_lib("exmpp/include/exmpp.hrl").
@@ -38,9 +40,9 @@ loop(XmppCom, JID, PubIP) ->
             io:format("~p~n", [Record]),
             process_message(XmppCom, Packet, JID),
             loop(XmppCom, JID, PubIP);
-	Record = #received_packet{packet_type=iq, raw_packet=IQ} ->
+	Record = #received_packet{packet_type=iq, type_attr=Type, raw_packet=IQ} ->
 	    io:format("~p~n", [Record]),
-	    process_iq(XmppCom, IQ, PubIP,exmpp_xml:get_ns_as_atom(exmpp_iq:get_payload(IQ))),
+	    process_iq(XmppCom, Type, IQ, PubIP,exmpp_xml:get_ns_as_atom(exmpp_iq:get_payload(IQ)),JID),
 	    loop(XmppCom, JID, PubIP);
 	Record ->
             io:format("~p~n", [Record]),
@@ -48,7 +50,7 @@ loop(XmppCom, JID, PubIP) ->
     end.
 
 %% Create Channel and return details
-process_iq(XmppCom, IQ, PubIP, ?NS_CHANNEL) ->
+process_iq(XmppCom, "get", IQ, PubIP, ?NS_CHANNEL, _) ->
     case allocate_relay() of
 	{A, B} ->
 		Result = exmpp_iq:result(IQ,get_candidate_elem(PubIP, A, B)),
@@ -58,19 +60,26 @@ process_iq(XmppCom, IQ, PubIP, ?NS_CHANNEL) ->
 		exmpp_component:send_packet(XmppCom, Error)
 	end; 
 
-process_iq(Session, "get", ?NS_DISCO_INFO, IQ) ->
-	Identity = exmpp_xml:element(?NS_DISCO_INFO, 'identity', [exmpp_xml:attribute("category", <<"proxy">>),
-				  		      exmpp_xml:attribute("type", <<"relay">>),
-						      exmpp_xml:attribute("name", <<"Jingle Nodes Relay">>)
-						      ],
-			 	     []),
-	IQRegisterFeature1 = exmpp_xml:element(?NS_DISCO_INFO, 'feature', [exmpp_xml:attribute('var', ?NS_JINGLE_NODES_s)],[]),
-	IQRegisterFeature2 = exmpp_xml:element(?NS_DISCO_INFO, 'feature', [exmpp_xml:attribute('var', ?NS_CHANNEL_s)],[]),
-	Result = exmpp_iq:result(IQ, exmpp_xml:element(?NS_DISCO_INFO, 'query', [], [Identity, IQRegisterFeature1, IQRegisterFeature2])),
-	exmpp_component:send_packet(Session, Result);
+process_iq(XmppCom, "get", IQ, _, ?NS_DISCO_INFO, _) ->
+        Identity = exmpp_xml:element(?NS_DISCO_INFO, 'identity', [exmpp_xml:attribute("category", <<"proxy">>),
+                                                      exmpp_xml:attribute("type", <<"relay">>),
+                                                      exmpp_xml:attribute("name", <<"Jingle Nodes Relay">>)
+                                                      ],
+                                     []),
+        IQRegisterFeature1 = exmpp_xml:element(?NS_DISCO_INFO, 'feature', [exmpp_xml:attribute('var', ?NS_JINGLE_NODES_s)],[]),
+        IQRegisterFeature2 = exmpp_xml:element(?NS_DISCO_INFO, 'feature', [exmpp_xml:attribute('var', ?NS_CHANNEL_s)],[]),
+        Result = exmpp_iq:result(IQ, exmpp_xml:element(?NS_DISCO_INFO, 'query', [], [Identity, IQRegisterFeature1, IQRegisterFeature2])),
+        exmpp_component:send_packet(XmppCom, Result);
 
-process_iq(XmppCom, IQ, _, _) ->
-		    Error = exmpp_iq:error(IQ),
+process_iq(XmppCom, "get", IQ, _, ?NS_JINGLE_NODES, JID) ->
+	Relay = exmpp_xml:element(undefined, 'relay', [exmpp_xml:attribute('policy',"public"), exmpp_xml:attribute('protocol',"udp"), exmpp_xml:attribute('address',JID)], []),
+	Services = exmpp_xml:element(?NS_JINGLE_NODES, ?NAME_SERVICES, [],[Relay]),
+	Result = exmpp_iq:result(IQ, Services),
+	io:format("~p~n", [Result]),
+	exmpp_component:send_packet(XmppCom, Result);
+
+process_iq(XmppCom, "get", IQ, _, _, _) ->
+		    Error = exmpp_iq:error(IQ,'feature-not-implemented'),
 		    exmpp_component:send_packet(XmppCom, Error).
 
 get_candidate_elem(Host, A, B) ->
