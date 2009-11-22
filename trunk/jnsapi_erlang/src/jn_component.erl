260c49ff3604b92defdc527cc21a8d322a5e6740
barata7@gmail.com <barata7@gmail.com@1b8834d4-a9fb-11de-8c46-7bd654735883>
2009-11-22 17:46:08 +0000

    [12] Purge Innactive Relay Functions Updates

--- trunk/jnsapi_erlang/src/jn_component.erl
+++ trunk/jnsapi_erlang/src/jn_component.erl
@@ -6,7 +6,7 @@
 %%%		* UDP Relay Services
 %%%
 %%% Created : 01 Nov 2009 by Thiago Camargo <barata7@gmail.com>
-%%% Example Usage: jn_component:start("jn.localhost", "secret", "localhost", 8888, "127.0.0.1").
+%%% Example Usage: jn_component:start("jn.localhost", "secret", "localhost", 8888, "127.0.0.1", 60000).
 %%%-------------------------------------------------------------------
 
 -module(jn_component).
@@ -18,31 +18,43 @@
 -define(NAME_SERVICES,'services').
 -define(NS_CHANNEL_s,"http://jabber.org/protocol/jinglenodes#channel").
 
+-define(ERROR_MSG(Format, Args),
+	error_logger:error_msg("(~p:~p:~p) " ++ Format ++ "~n",
+			       [self(), ?MODULE, ?LINE | Args])).
+
+-define(INFO_MSG(Format, Args),
+	error_logger:info_msg("(~p:~p:~p) " ++ Format ++ "~n",
+			       [self(), ?MODULE, ?LINE | Args])).
+
 -include_lib("exmpp/include/exmpp.hrl").
 -include_lib("exmpp/include/exmpp_client.hrl").
 
--export([start/5, stop/1]).
--export([init/5]).
+-export([start/6, stop/1]).
+-export([init/6]).
 
 -record(relay, {pid, user}).
 
-start(JID, Pass, Server, Port, PubIP) ->
-	   spawn(?MODULE, init, [JID, Pass, Server, Port, PubIP]).
+start(JID, Pass, Server, Port, PubIP, ChannelTimeout) ->
+	   spawn(?MODULE, init, [JID, Pass, Server, Port, PubIP, ChannelTimeout]).
 
 stop(JNComPid) ->
     JNComPid ! stop.
 
-init(JID, Pass, Server, [_|_]=Port, PubIP) ->
+init(JID, Pass, Server, [_|_]=Port, PubIP, ChannelTimeout) ->
 	  {ok, [NPort], _} = io_lib:fread("~u",Port),
-	  init(JID, Pass, Server, NPort, PubIP);
+	  init(JID, Pass, Server, NPort, PubIP, ChannelTimeout);
 
-init(JID, Pass, Server, Port, PubIP) ->
+init(JID, Pass, Server, Port, PubIP, [_|_]=ChannelTimeout) ->
+	  {ok, [NTimeout], _} = io_lib:fread("~u", ChannelTimeout),
+	  init(JID, Pass, Server, Port, PubIP, NTimeout);
+
+init(JID, Pass, Server, Port, PubIP, ChannelTimeout) ->
     application:start(exmpp),
     XmppCom = exmpp_component:start(),
     exmpp_component:auth(XmppCom, JID, Pass),
     _StreamId = exmpp_component:connect(XmppCom, Server, Port),
     exmpp_component:handshake(XmppCom),
-    ChannelMonitor = scheduleChannelPurge(3000, [], 10000),
+    ChannelMonitor = scheduleChannelPurge(5000, [], ChannelTimeout),
     loop(XmppCom, JID, PubIP, ChannelMonitor).
 
 loop(XmppCom, JID, PubIP, ChannelMonitor) ->
@@ -51,15 +63,15 @@ loop(XmppCom, JID, PubIP, ChannelMonitor) ->
             exmpp_component:stop(XmppCom);
         %% If we receive a message, we reply with the same message
         Record = #received_packet{packet_type=message, raw_packet=Packet} ->
-            io:format("~p~n", [Record]),
+	    ?INFO_MSG("Message Received: ", [Record]),
             process_message(XmppCom, Packet, JID),
             loop(XmppCom, JID, PubIP, ChannelMonitor);
 	Record = #received_packet{packet_type=iq, type_attr=Type, raw_packet=IQ} ->
-	    io:format("~p~n", [Record]),
+	    ?INFO_MSG("IQ Request: ", [Record]),
 	    process_iq(XmppCom, Type, IQ, PubIP,exmpp_xml:get_ns_as_atom(exmpp_iq:get_payload(IQ)),JID, ChannelMonitor),
 	    loop(XmppCom, JID, PubIP, ChannelMonitor);
 	Record ->
-            io:format("~p~n", [Record]),
+            ?INFO_MSG("Unknown Request: ", [Record]),
             loop(XmppCom, JID, PubIP, ChannelMonitor)
     end.
 
@@ -87,7 +99,7 @@ process_iq(XmppCom, "get", IQ, _, ?NS_DISCO_INFO, _, _) ->
         exmpp_component:send_packet(XmppCom, Result);
 
 process_iq(XmppCom, "get", IQ, _, ?NS_JINGLE_NODES, JID, _) ->
-	Relay = exmpp_xml:element(undefined, 'relay', [exmpp_xml:attribute('policy',"public"), exmpp_xml:attribute('protocol',"udp"), exmpp_xml:attribute('address',JID)], []),
+	Relay = exmpp_xml:element(undefined, 'relay', [exmpp_xml:attribute('policy',"public"), exmpp_xml:attribute('protocol', "udp"), exmpp_xml:attribute('address', JID)], []),
 	Services = exmpp_xml:element(?NS_JINGLE_NODES, ?NAME_SERVICES, [],[Relay]),
 	Result = exmpp_iq:result(IQ, Services),
 	io:format("~p~n", [Result]),
@@ -113,23 +125,25 @@ process_message(XmppCom, Packet, JID) ->
     exmpp_component:send_packet(XmppCom, NewPacket).
 
 allocate_relay(ChannelMonitor, U) -> allocate_relay(ChannelMonitor, U, 10000,10).
-allocate_relay(_, _, _, 0) -> {error, null};
+allocate_relay(_, U, _, 0) -> 
+	 ?ERROR_MSG("Could Not Allocate Port for : ~p", [U]),
+	{error, null};
 allocate_relay(ChannelMonitor, U, I, Tries) ->
-     case udp_relay:start(I,I+2) of
+     case udp_relay:start(I, I+2) of
 	{ok, R} -> 
 		ChannelMonitor ! #relay{pid=R, user=U},
-		{I,I+2};
-	_ -> allocate_relay(ChannelMonitor, U, I+3,Tries-1)
+		{I, I+2};
+	_ -> allocate_relay(ChannelMonitor, U, I+3, Tries-1)
      end.
 
-check_relay(#relay{pid= PID}, Timeout) ->
+check_relay(#relay{pid= PID, user=U}, Timeout) ->
 	io:format("Checking: ~p~n", [PID]),
-	T = gen_server:call(PID, get_timestamp),
-	io:format("GS_CALL: ~p~n", [T]),
+	T = gen_server:call(PID, get_timestamp),	
 	Delta = timer:now_diff(now(), T)/1000,
 	if
 	Delta > Timeout ->
-		exit(PID,kill),
+		?INFO_MSG("Channel Killed: ", [U]),
+		exit(PID, kill),
 		removed;
 	true -> 
 		ok
