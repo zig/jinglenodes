f29d8c5304be113b7b5c8fce0678d5f53378d5ad
barata7 <barata7@gmail.com>
2010-05-21 06:39:57 +0000

    Start and Stop Scripts Update

--- jnsapi_erlang/start.sh
+++ jnsapi_erlang/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-echo "Usage: ./start.sh jn.localhost secret localhost 8888 192.168.20.189 60000 localhost 2 60 10000 60000"
-CMD="erl -sname JNLocal -pa $PWD/ebin -heart -run jn_component start $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} -detached -noinput -noshell"
+##echo "Usage: ./start.sh jn.localhost secret localhost 8888 192.168.20.189 60000 localhost 2 60 10000 60000"
+echo $PWD
+CMD="erl -sname JNLocal -pa $PWD/ebin -s app_jn_component permanent 1" ##-detached -noinput -noshell"
 export HEART_COMMAND=$CMD
 exec $CMD
