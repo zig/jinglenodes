5fdda152987524f416fb6bd0d1d417d7c52936c5
barata7@gmail.com <barata7@gmail.com@1b8834d4-a9fb-11de-8c46-7bd654735883>
2010-05-26 20:24:35 +0000

    Fixed Bugs Reported by Rodrigo with Patch ERL-BUILD-FIX

--- trunk/jnsapi_erlang/stop.sh
+++ trunk/jnsapi_erlang/stop.sh
@@ -1,2 +1,7 @@
-erl_call -n JNLocal -v -a 'init stop []'
-
+#!/bin/bash
+HOSTNAME=d6h57b4j0
+##`hostname`
+erl_call -n JNLocal@$HOSTNAME -v -a 'init stop []'
+kill -9 `ps -C epmd -o pid=`
+kill -9 `ps -C beam.smp -o pid=`
+kill -9 `ps -C heart -o pid=`
