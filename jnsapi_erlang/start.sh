92e2603d43e0a6a1e701c9efd030b50a947903a4
barata7 <barata7@gmail.com>
2010-10-23 14:00:19 +0000

    Prepare for new Contributed Version

--- jnsapi_erlang/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo $PWD
-CMD="erl -sname JNLocal -pa $PWD/ebin -s app_jn_component -detached -noinput -noshell"
-export HEART_COMMAND=$CMD
-exec $CMD
--- jnsapi_erlang/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-HOSTNAME=`hostname`
-##`hostname`
-erl_call -n JNLocal@$HOSTNAME -v -a 'init stop []'
-kill -9 `ps -C epmd -o pid=`
-kill -9 `ps -C beam.smp -o pid=`
-kill -9 `ps -C heart -o pid=`
