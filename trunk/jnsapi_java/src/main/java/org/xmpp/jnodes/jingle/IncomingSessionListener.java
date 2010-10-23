1d4512e386180c3ea8fb4f11827e84bd083dbc97
barata7@gmail.com <barata7@gmail.com@1b8834d4-a9fb-11de-8c46-7bd654735883>
2010-10-23 13:59:35 +0000

    Not Applicable

--- trunk/jnsapi_java/src/main/java/org/xmpp/jnodes/jingle/IncomingSessionListener.java
+++
@@ -1,5 +0,0 @@
-package org.xmpp.jnodes.jingle;
-
-public interface IncomingSessionListener {
-    public void sessionReceived(final JingleSession session);
-}
--- trunk/jnsapi_java/src/main/java/org/xmpp/jnodes/jingle/RawUdpTransportProvider.java
+++
@@ -1,7 +0,0 @@
-package org.xmpp.jnodes.jingle;
-
-import java.util.List;
-
-public interface RawUdpTransportProvider {
-    public void setupTransport(final List<JingleContent> contents);
-}
