6305a26a65f761c6c3fb0bea766a27db77b45df6
barata7@gmail.com <barata7@gmail.com@1b8834d4-a9fb-11de-8c46-7bd654735883>
2012-02-01 18:48:11 +0000

    Fixed Start Script to match Stop erl Name

--- trunk/jnsapi_erlang/start.sh
+++ trunk/jnsapi_erlang/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-erl -sname JingleNodes -pa $PWD/ebin -s app_jn_component -detached -noinput -noshell -env ERL_MAX_PORTS 65000
+erl -name JingleNodes -pa $PWD/ebin -s app_jn_component -detached -noinput -noshell -env ERL_MAX_PORTS 65000
